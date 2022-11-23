f1918c802b7fadd0d2a1fda603431fa324b7f1e7
imran <imranaha310@gmail.com>
2022-11-23 22:00:17 +0530

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,48 @@ import "../Styles/Navbar.css";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  const linkStyle={
+    fontWeight:"bold",
+    textDecoration:"none",
+    color:"white"
+  }
+
+  const responsive_Menu=()=>{
+    var e=document.getElementById('NavResponsive');
+    if(e.className==='navbar'){
+      e.className+=' responsive';
+    }
+    else{
+      e.className='navbar';
+    }
+  }
   return (
+    
     <div>
       <nav>
+      <div className="Nav-Hidden"onClick={responsive_Menu}>
+        <div id="bar1"className="bar"></div>
+        <div id="bar2"className="bar"></div>
+        <div id="bar3"className="bar"></div>
+      </div>
         <div className="logo">
           <h1>React projects</h1>
         </div>
-        <div className="navbar">
+        <div className="navbar"id="NavResponsive">
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/"style={linkStyle}>Home</Link>
             </li>
             <li>
-              <Link to="/Counterclick">Counterclick</Link>
+              <Link to="/Counterclick"style={linkStyle}>Counterclick</Link>
             </li>
             <li>
-              <Link to="/Clock">Clock</Link>
+              <Link to="/Clock"style={linkStyle}>Clock</Link>
             </li>
           </ul>
         </div>
       </nav>
+     
     </div>
   );
 };
